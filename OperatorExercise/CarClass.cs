ebb1f1c9a9ac1c875086f1276664acb9fc4d82bf
Bdotgo17 <bdotgo@hotmail.com>
2022-09-18 14:03:24 -0500

    Initial

---
+++ OperatorExercise/CarClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace OperatorExercise
+{
+    public class CarClass
+    {
+        public CarClass()
+        {
+
+        }
+
+        public CarClass(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+
+    }
+}
+
---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarClass myCar = new CarClass();
+            myCar.Make = "Ford";
+            myCar.Model = "Focus";
+            myCar.Year = 2013;
+
+            var mazda = new CarClass()
+            {
+                Make = "Mazda",
+                Model = "CX-5",
+                Year = 2016
+            };
+
+            var chevy = new CarClass("Chevy", "Impala", 2001);
+
+            //var carList = new List<CarClass>() { myCar, mazda, chevy};
+
+            var carList = new List<CarClass>() { myCar, mazda, chevy};
+            //var Console.WriteLine(carList.Capacity);
+
+            //Console.WriteLine(carList.Capacity);
+
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Model} {vehicle.Year}");
+            }
+
+
+
+        }
+    }
+}
+
